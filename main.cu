f1d0267a87d6a6b3d897b962bd1e367730050428
mnbarrio <mauricio.barriosp@sansano.usm.cl>
2021-12-14 00:50:11 -0300

    versión final esta si que si

--- main.cu
+++ main.cu
@@ -70,6 +70,30 @@ void Write(float* arr, int Nx, int Ny, const char *filename) {
  */
 __global__ void KernelGPU(const float* array, float* arrayp1, const int Nx, const int Ny, const float dx2, const float dy2, const float alpha, const float dt){
 
+
+  int i = threadIdx.x + blockIdx.x*blockDim.x;
+  if (i > 0 && i < Nx - 1){
+    int j = threadIdx.y + blockIdx.y*blockDim.y;
+    if (j > 0 && j < Ny - 1){
+
+      float u_ij = array[getIndex(i,j,Ny)];
+      float u_im1j = array[getIndex(i-1, j, Ny)];
+      float u_ip1j = array[getIndex(i+1, j, Ny)];
+      float u_ijm1 = array[getIndex(i, j-1, Ny)];
+      float u_ijp1 = array[getIndex(i, j+1, Ny)];
+
+      arrayp1[getIndex(i, j, Ny)] = u_ij + alpha * dt * ( (u_im1j - 2.0*u_ij + u_ip1j)/dx2 + (u_ijm1 - 2.0*u_ij + u_ijp1)/dy2 );
+    }
+  }
+  // __syncthreads();
+
+}
+
+/*
+ *  Kernel 2 (Shared Memory)
+ */
+__global__ void KernelGPU2(const float* array, float* arrayp1, const int Nx, const int Ny, const float dx2, const float dy2, const float alpha, const float dt){
+
   __shared__ float s_Array[(block_size_x + 2)*(block_size_y + 2)];
   int i = threadIdx.x + blockIdx.x*blockDim.x;
   int j = threadIdx.y + blockIdx.y*blockDim.y;
@@ -82,25 +106,24 @@ __global__ void KernelGPU(const float* array, float* arrayp1, const int Nx, cons
   s_Array[getIndex(s_i,s_j,s_ny)] = array[getIndex(i,j,Ny)];
 
   // Borde superior
-  if (s_i == 1 && s_j != 1 && i != 0){
+  if (s_i == 1 && s_j != 0 && i != 0){
     s_Array[getIndex(0, s_j, s_ny)] = array[getIndex(blockIdx.x*blockDim.x - 1, j, Ny)];
   }
 
   // Borde inferior
-  if (s_i == block_size_x && s_j != block_size_y + 1  && i != Nx -1){
+  if (s_i == block_size_x  && s_j != block_size_y + 1  && i != Nx -1){
     s_Array[getIndex(block_size_x + 1, s_j, s_ny)] = array[getIndex((blockIdx.x + 1)*blockDim.x, j, Ny)];
   }
 
   // Borde izquierdo
-  if (s_i != 1 && s_j==1 && j != 0){
+  if (s_i != 0 && s_j==1 && j != 0){
     s_Array[getIndex(s_i, 0, s_ny)] = array[getIndex(i, blockIdx.y*blockDim.y -1, Ny)]; 
   }
 
   // Borde derecho
-  if (s_i != block_size_x && s_j == block_size_y && j != Ny-1){
+  if (s_i != block_size_x + 1  && s_j == block_size_y && j != Ny-1){
     s_Array[getIndex(s_i, block_size_y+1, s_ny)] = array[getIndex(i, (blockIdx.y + 1)*blockDim.y, Ny)];
   }
-
   __syncthreads();
 
   if (i > 0 && i < Nx - 1){
@@ -111,44 +134,11 @@ __global__ void KernelGPU(const float* array, float* arrayp1, const int Nx, cons
       float u_ijm1 = s_Array[getIndex(s_i, s_j - 1, s_ny)];
       float u_ijp1 = s_Array[getIndex(s_i, s_j + 1, s_ny)];
 
-      // float u_ij   = s_Array[getIndex(i,j,Ny)];
-      // float u_im1j = s_Array[getIndex(i-1, j, Ny)];
-      // float u_ip1j = s_Array[getIndex(i+1, j, Ny)];
-      // float u_ijm1 = s_Array[getIndex(i, j-1, Ny)];
-      // float u_ijp1 = s_Array[getIndex(i, j+1, Ny)];
-
-      arrayp1[getIndex(i, j, Ny)] = u_ij + alpha * dt * ( (u_im1j - 2.0*u_ij + u_ip1j)/dx2 + (u_ijm1 - 2.0*u_ij + u_ijp1)/dy2 );
+      arrayp1[getIndex(i, j, Ny)] =  u_ij + alpha * dt * ( (u_im1j - 2.0*u_ij + u_ip1j)/dx2 + (u_ijm1 - 2.0*u_ij + u_ijp1)/dy2 );
     }
   }
+
 }
-
-/*
- *  Kernel 2 (Shared Memory)
- */
-// __global__ void KernelGPU2(const float* array, float* arrayp1, const int Nx, const int Ny, const float dx2, const float dy2, const float alpha, const float dt){
-
-//   __shared__ float s_Array[block_size_x + 2][block_size_y +2];
-
-//   //Filas y columnas del bloque
-//   int row = threadIdx.x + blockIdx.x*blockDim.x;
-//   int col = threadIdx.y + blockIdx.y*blockDim.y;
-
-//   //Fila y columna de la memoria compartida
-//   int i = threadIdx.x + 1; // sin contar el borde superior 
-//   int j = threadIdx.y + 1; // sin contar el borde izquierdo 
-//   int ny = block_size_y +2; // Cantidad de columnas del sub-placa
-
-//   //Rellenar memoria compartida
-//   //Cuadro central
-//   s_Array[getIndex(i,j,ny)] = array[getIndex(row,col,ny)];
-
-
-
-
-
-
-
-// }
 
 
 /*
@@ -179,11 +169,11 @@ int main(int argc, char **argv){
   const float dt = dx2*dy2 / (2.0 * alpha * (dx2 + dy2)); // delta tiempo
   // float dt = 0.00001;
 
-  const int t = 1000; // cantidad de iteraciones
-  const int outputEveryTime = 200;
+  const int t = 5000; // cantidad de iteraciones
+  const int outputEveryTime = 100;
 
   float Tedge = 25.0;
-  float Tcenter = 200.0;
+  float Tcenter = 500.0;
   // float radius = 5;
   float radius = (Nx/6.0) * (Nx/6.0); //m 
 
@@ -203,33 +193,40 @@ int main(int argc, char **argv){
 
   memcpy(Ahostp1, Ahost, nElements*sizeof(float));
 
-//--------------- variables de GPU ------------------- 
+  // Varibles para sacar tiempo promedio
+  float sum = 0;
+  float iter = 0;
 
 //----------------------- CPU ------------------------
-  t1 = clock();
-  
-  /*for (int n = 0; n <= t; n++){
+
+  for (int n = 0; n <= t; n++){
     
+    t1 = clock();
     Heat_transmition(Ahost, Ahostp1, Nx, Ny, alpha, dx, dy, dt);
+    t2 = clock();
+
+    ms = 1000.0 * (double)(t2 - t1) / CLOCKS_PER_SEC;
+
+    sum = sum + ms;
+    iter ++;
     
     // Printea el arreglo cada 20 iteraciones.
 		if (n % outputEveryTime == 0){
       int i = n/outputEveryTime;
       char filename[64];
-      sprintf(filename, "archivos/transmition_CPU_%03d.txt", i);
+      sprintf(filename, "archivos/CPU/transmition_CPU_%03d.txt", i);
       Write(Ahost, Nx, Ny, filename);
 		}
     swap(Ahost, Ahostp1);
 
-  }*/
+  }
 
-  t2 = clock();
-  ms = 1000.0 * (double)(t2 - t1) / CLOCKS_PER_SEC;
-  printf("\nTiempo CPU: %f[ms]\n", ms);
+  
+  float t_prom = sum/iter;
+  printf("Tiempo CPU: %f[ms]\n", t_prom);
 
 //-------------------- kernel 1 -----------------------
 //---------------- variables de GPU -------------------
-
 
   float* Bhost;
   float* Bhostp1;
@@ -243,36 +240,82 @@ int main(int argc, char **argv){
   dim3 gs(Nx/block_size_x + 1, Ny/block_size_y +1); //gs
   dim3 bs(block_size_x, block_size_y); // bs
 
-  printf("GS: {%d %d %d}\n", gs.x, gs.y, gs.z);
-  printf("BS: {%d %d %d}\n", bs.x, bs.y, bs.z);
-
-  // printf("GS: %d\n", gs);
-
-  // float* Ahostgpu = new float[nElements];
-
-  cudaEventCreate(&ct1);
-  cudaEventCreate(&ct2);
-  cudaEventRecord(ct1);
+  sum = 0;
+  iter = 0;
   for (int n = 0; n <= t; n++){
 
+    cudaEventCreate(&ct1);
+    cudaEventCreate(&ct2);
+    cudaEventRecord(ct1);
+
     KernelGPU<<<gs, bs>>>(Bhost, Bhostp1, Nx, Ny, dx2, dy2, alpha, dt);
+
+    cudaEventRecord(ct2);
+    cudaEventSynchronize(ct2);
+    cudaEventElapsedTime(&d_time, ct1, ct2);
+
+    sum = sum + d_time;
+    iter ++;
+
     // Printea el arreglo cada 20 iteraciones.
 		if (n % outputEveryTime == 0){
       cudaMemcpy(AhostGPU, Bhost, nElements*sizeof(float), cudaMemcpyDeviceToHost);
 
       int i = n/outputEveryTime;
       char filename[64];
-      sprintf(filename, "archivos/transmition_GPU_%03d.txt", i);
+      sprintf(filename, "archivos/GPU/K1/transmition_GPU_%03d.txt", i);
+      Write(AhostGPU, Nx, Ny, filename);
+		}
+
+    std::swap(Bhost, Bhostp1);
+  }
+  t_prom = sum/iter;
+  printf("Tiempo para GPU Kernel 1: %f [ms]\n", t_prom);
+  
+	cudaFree(Bhost); cudaFree(Bhostp1);
+
+//-------------------- kernel 2 -----------------------
+//---------------- variables de GPU -------------------
+
+  GenArray(&AhostGPU, Nx, Ny, Tedge, Tcenter, radius);
+
+  cudaMalloc((void**)&Bhost, nElements*sizeof(float));
+  cudaMalloc((void**)&Bhostp1, nElements*sizeof(float));
+
+  cudaMemcpy(Bhost, AhostGPU, nElements*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(Bhostp1, AhostGPU, nElements*sizeof(float), cudaMemcpyHostToDevice);
+
+  sum = 0;
+  iter = 0;
+
+  for (int n = 0; n <= t; n++){
+    cudaEventCreate(&ct1);
+    cudaEventCreate(&ct2);
+    cudaEventRecord(ct1);
+
+    KernelGPU2<<<gs, bs>>>(Bhost, Bhostp1, Nx, Ny, dx2, dy2, alpha, dt);
+
+    cudaEventRecord(ct2);
+    cudaEventSynchronize(ct2);
+    cudaEventElapsedTime(&d_time, ct1, ct2);
+    sum = sum + d_time;
+    iter ++;
+
+    // Printea el arreglo cada 20 iteraciones.
+		if (n % outputEveryTime == 0){
+      cudaMemcpy(AhostGPU, Bhost, nElements*sizeof(float), cudaMemcpyDeviceToHost);
+
+      int i = n/outputEveryTime;
+      char filename[64];
+      sprintf(filename, "archivos/GPU/K2/transmition_GPU_%03d.txt", i);
       Write(AhostGPU, Nx, Ny, filename);
 		}
 
     std::swap(Bhost, Bhostp1);
   }
 
-  cudaEventRecord(ct2);
-  cudaEventSynchronize(ct2);
-  cudaEventElapsedTime(&d_time, ct1, ct2);
-  printf("Tiempo para GPU: %f [ms]\n", d_time);
+  t_prom = sum/iter;
+  printf("Tiempo para GPU Kernel 2: %f [ms]\n", t_prom);
   
 	cudaFree(Bhost); cudaFree(Bhostp1); 
   delete[] Ahost; delete[] Ahostp1; 
